8e33bbe4592661457687699e41aa54936555f847
abhi16180 <64468853+abhi16180@users.noreply.github.com>
2021-09-11 23:44:15 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'dart:convert';
 import 'package:customdb/usermodel/user.dart';
 import 'package:customdb/usermodel/userlist.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
     home: App(),
+    theme: ThemeData(
+      brightness: Brightness.dark,
+    ),
   ));
 }
 
@@ -22,28 +25,28 @@ UserList? userlistInstance;
 class _AppState extends State<App> {
   // ignore: prefer_typing_uninitialized_variables
   var resp;
-  var userDataList;
+  var userDataList = [];
   var client = http.Client();
+  TextEditingController userController = new TextEditingController();
+  TextEditingController emailController = new TextEditingController();
   Future _fetchData() async {
     resp = await client
-        .get(Uri.parse('https://boiling-caverns-84229.herokuapp.com/get'));
+        .get(Uri.parse('https://pacific-beyond-59033.herokuapp.com/get'));
     userlistInstance = UserList.fromJson(jsonDecode(resp.body) as List);
-    userDataList = userlistInstance!.userList;
+    userDataList = userlistInstance!.userList as List;
   }
 
-  Future add() async {
-    var jsonData = User(username: 'username', email: 'email email').toJson();
+  Future add(String uname, String email) async {
+    var jsonData = User(username: uname, email: email).toJson();
     var uriResponse = await client.post(
-      Uri.parse('https://boiling-caverns-84229.herokuapp.com/add'),
-      body: jsonEncode(jsonData),
+      Uri.parse('https://pacific-beyond-59033.herokuapp.com/add'),
+      body: jsonData,
     );
-
-    print(uriResponse.body.toString());
   }
 
   Future deleteData() async {
-    await client.delete(
-        Uri.parse('https://boiling-caverns-84229.herokuapp.com/delete'));
+    await client
+        .delete(Uri.parse('https://pacific-beyond-59033.herokuapp.com/delete'));
   }
 
   @override
@@ -55,13 +58,14 @@ class _AppState extends State<App> {
         builder: (context, snap) {
           if (snap.connectionState == ConnectionState.done) {
             return ListView.builder(
-                physics: AlwaysScrollableScrollPhysics(),
+                physics: const AlwaysScrollableScrollPhysics(),
                 scrollDirection: Axis.vertical,
                 shrinkWrap: true,
                 itemCount: userDataList.length,
                 itemBuilder: (context, item) {
                   return ListTile(
                     title: Text(userDataList[item].username.toString()),
+                    subtitle: Text(userDataList[item].email.toString()),
                   );
                 });
           } else {
@@ -69,36 +73,74 @@ class _AppState extends State<App> {
           }
         },
       ),
-      floatingActionButton: Column(
+      floatingActionButton: SpeedDial(
+        child: const Icon(Icons.more),
         children: [
-          const SizedBox(
-            height: 100,
-          ),
-          FloatingActionButton(
-            onPressed: () async {
-              await add();
-            },
-            child: const Icon(Icons.add),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FloatingActionButton(
-            onPressed: () async {
-              await deleteData();
-              setState(() {});
-            },
-            child: const Icon(Icons.delete),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              setState(() {});
-            },
+          SpeedDialChild(
+              child: const Icon(Icons.add),
+              onTap: () async {
+                showDialog(
+                    context: context,
+                    builder: (contex) {
+                      return AlertDialog(
+                        title: Center(child: Text('Add user details here')),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        actions: [
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          TextField(
+                            controller: userController,
+                            decoration: InputDecoration(
+                              hintText: 'Username',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide:
+                                    const BorderSide(color: Colors.amber),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 50,
+                          ),
+                          TextField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide:
+                                    const BorderSide(color: Colors.amber),
+                              ),
+                            ),
+                          ),
+                          Center(
+                            child: MaterialButton(
+                              child: Text('ADD'),
+                              onPressed: () async {
+                                await add(
+                                    userController.text, emailController.text);
+                                Navigator.of(context).pop();
+                                setState(() {});
+                              },
+                            ),
+                          )
+                        ],
+                      );
+                    });
+              }),
+          SpeedDialChild(
+              child: const Icon(Icons.delete),
+              onTap: () async {
+                await deleteData();
+                setState(() {});
+              }),
+          SpeedDialChild(
             child: const Icon(Icons.refresh),
-          ),
+            onTap: () => setState(() {}),
+          )
         ],
       ),
     );
